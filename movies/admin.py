33d1c9330580868daa1357f211f585939b42717a
Mark <m_antoniadis@live.com>
2021-05-11 20:43:11 -0400

    revamped the models and added images to movie model

--- movies/admin.py
+++ movies/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Series, Genre
 
 
 class MoviesAdmin(admin.ModelAdmin):
     model = Movie
-    list_display = ['name', 'series_name', 'movie_order']
-    fields = ['name', 'release_date', 'series_name', 'movie_order']
+    list_display = ['name', 'series', 'movie_order', 'image']
+    fields = ['name', 'release_date', 'series', 'movie_order', 'image']
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    model = Series
+    list_display = ['name', 'description', 'get_genre']
+    fields = ['name', 'description', 'genre']
+
+    def get_genre(self, obj):
+        return obj.genre.name
+
+    get_genre.short_description = 'Genre'
+    get_genre.admin_order_field = 'genre__name'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+    list_display = ['name']
+    fields = ['name']
 
 
 admin.site.register(Movie, MoviesAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Genre, GenreAdmin)
 
--- movies/models.py
+++ movies/models.py
@@ -1,12 +1,42 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=500)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movie_genres')
+
+    def __str__(self):
+        return self.name
+
+
+def movie_image_path(instance, filename):
+    series = str(instance.series.name).replace(" ", "_")
+    return 'movies/static/movies/img/{0}/{1}'.format(series, filename)
+
+
 class Movie(models.Model):
     name = models.CharField(max_length=100, null=False)
     description = models.TextField(max_length=500)
     release_date = models.DateField()
     directed_by = models.CharField(max_length=50)
-    series_name = models.CharField(max_length=50)
-    movie_order = models.IntegerField()
-    str
+    series = models.ForeignKey(Series, on_delete=models.PROTECT, related_name='movie_to_series')
+    movie_order = models.IntegerField(null=True)
+    image = models.ImageField(upload_to=movie_image_path, height_field=None, width_field=None, max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
 
